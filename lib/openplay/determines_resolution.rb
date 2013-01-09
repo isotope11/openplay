1cf68bed542747c5b03404345edb616c46e06e6c
Seth Messer <seth@isotope11.com>
2013-01-09 15:56:21 -0600

    Support resolution finding on a mac

--- lib/openplay/determines_resolution.rb
+++ lib/openplay/determines_resolution.rb
@@ -1,6 +1,20 @@
 class DeterminesResolution
   def get
+    if RUBY_PLATFORM =~ /darwin/
+      get_mac
+    else
+      get_linux
+    end
+  end
+
+  private
+  def get_linux
     x, y = `xrandr`.scan(/current (\d+) x (\d+)/).flatten
+    "#{x}x#{y}"
+  end
+
+  def get_mac 
+    x, y = `osascript -e 'tell application "Finder" to get bounds of window of desktop'`.split(", ")[2..3]
     "#{x}x#{y}"
   end
 end
