f971b8b7197319f63289b479de3701118a129cdf
Josh Adams <josh@isotope11.com>
2013-01-09 15:39:02 -0600

    Support Mac for server?

--- lib/openplay/server.rb
+++ lib/openplay/server.rb
@@ -10,12 +10,20 @@ module Openplay
     end
 
     def receive
-      `cvlc udp://@:#{port} --network-caching=250`
+      `#{cvlc_command} udp://@:#{port} --network-caching=250`
     end
 
     private
     def announce_availability
       DNSSD.register `hostname`.chomp, "_openplay._tcp", nil, port
+    end
+
+    def cvlc_command
+      if RUBY_PLATFORM =~ /darwin/
+        "/Applications/VLC.app/Contents/MacOS/VLC -I rc"
+      else
+        "cvlc"
+      end
     end
   end
 end
